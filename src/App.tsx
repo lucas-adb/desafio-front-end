8566b5931d2217cee02f81027e27d9d9989deb84
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-04-02 15:45:43 -0300

    feat: adds Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,91 +3,92 @@ import "./App.css";
 import ChevronDownBlue from "./assets/icons/chevron-down-blue.svg";
 import ProfilePhoto from "./assets/images/profile-test.png";
 import SearchIcon from "./assets/icons/search-icon-gray.svg";
+import { Header } from "./components/Header";
 
 function App() {
   const [showInfo, setShowInfo] = useState(false);
 
   return (
-    <main className="main">
-      <div className="main__container">
-        <h1 className="h1">Funcionários</h1>
+    <>
+      <Header />
 
-        <div className="search-container box-shadow-2">
-          <input
-            type="text"
-            placeholder="Pesquisar"
-            className="h3"
-          />
-          <img src={SearchIcon} alt="search-icon" />
+      <main className="main">
+        <div className="main__container">
+          <h1 className="h1">Funcionários</h1>
+
+          <div className="search-container box-shadow-2">
+            <input type="text" placeholder="Pesquisar" className="h3" />
+            <img src={SearchIcon} alt="search-icon" />
+          </div>
+
+          <div>
+            <table className="table--mobile box-shadow-2">
+              <thead className="thead--mobile box-shadow-1">
+                <tr>
+                  <th>FOTO</th>
+                  <th>NOME</th>
+                  <th>
+                    <div className="th__ellipse-div"></div>
+                  </th>
+                </tr>
+              </thead>
+
+              <tbody className="tbody--mobile">
+                <tr>
+                  <td>
+                    <img
+                      src={ProfilePhoto}
+                      alt="profile-picture"
+                      className="tbody__profile-picture"
+                    />
+                  </td>
+                  <td>Giovana L. Arruda</td>
+                  <td>
+                    <button
+                      className="chevron-container-btn"
+                      onClick={() => setShowInfo((prevstate) => !prevstate)}
+                    >
+                      <img src={ChevronDownBlue} alt="chevron-down" />
+                    </button>
+                  </td>
+                </tr>
+                {showInfo && (
+                  <>
+                    <tr className="hidden-tr">
+                      <td colSpan={3}>
+                        <div className="hidden-tr__div">
+                          <p className="h2">Cargo:</p>
+                          <p>Front-end</p>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr className="hidden-tr">
+                      <td colSpan={3}>
+                        <div className="hidden-tr__div">
+                          <p className="h2">Data de admissão:</p>
+                          <p>00/00/0000</p>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr className="hidden-tr">
+                      <td colSpan={3}>
+                        <div className="hidden-tr__div">
+                          <p className="h2">Telefone:</p>
+                          <p>+55 (55) 55555-555</p>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr className="hidden-tr__empty-row">
+                      <td></td>
+                    </tr>
+                  </>
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
-
-        <div>
-          <table className="table--mobile box-shadow-2">
-            <thead className="thead--mobile box-shadow-1">
-              <tr>
-                <th>FOTO</th>
-                <th>NOME</th>
-                <th>
-                  <div className="th__ellipse-div"></div>
-                </th>
-              </tr>
-            </thead>
-
-            <tbody className="tbody--mobile">
-              <tr>
-                <td>
-                  <img
-                    src={ProfilePhoto}
-                    alt="profile-picture"
-                    className="tbody__profile-picture"
-                  />
-                </td>
-                <td>Giovana L. Arruda</td>
-                <td>
-                  <button
-                    className="chevron-container-btn"
-                    onClick={() => setShowInfo((prevstate) => !prevstate)}
-                  >
-                    <img src={ChevronDownBlue} alt="chevron-down" />
-                  </button>
-                </td>
-              </tr>
-              {showInfo && (
-                <>
-                  <tr className="hidden-tr">
-                    <td colSpan={3}>
-                      <div className="hidden-tr__div">
-                        <p className="h2">Cargo:</p>
-                        <p>Front-end</p>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr className="hidden-tr">
-                    <td colSpan={3}>
-                      <div className="hidden-tr__div">
-                        <p className="h2">Data de admissão:</p>
-                        <p>00/00/0000</p>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr className="hidden-tr">
-                    <td colSpan={3}>
-                      <div className="hidden-tr__div">
-                        <p className="h2">Telefone:</p>
-                        <p>+55 (55) 55555-555</p>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr className="hidden-tr__empty-row">
-                    <td></td>
-                  </tr>
-                </>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import BeLogo from "../assets/brand/be-logo.svg";
+
+export function Header() {
+  return (
+    <header className="header box-shadow-2">
+    <nav>
+      <a href="/">
+        <img src={BeLogo} alt="be-logo" />
+      </a>
+    </nav>
+  </header>
+  )
+}
